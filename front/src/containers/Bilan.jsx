5c8a22534369f8699e5fa7cdbd0d5a4f27c3669c
fjulien <fettingerjulien@yahoo.fr>
2018-12-20 23:44:50 +0100

    encore un commit les enfants

--- front/src/containers/Bilan.jsx
+++ front/src/containers/Bilan.jsx
@@ -19,9 +19,40 @@ class Bilan extends Component {
             <p>Renouvelement équipement : fort</p>
             <p>Bilan carbone : 50 tCO2eq par salarié</p>
           </div>
+          <div>
+            <img src='' alt='image' />
+          </div>
         </div>
         <div className='conseil_perso'>
           <h2 className='title'>Conseils personnalisés</h2>
+
+          <div className='conseil_liste'>
+            <NavLink to='/conseil' className='groopCard'>
+              <img className={"conseil"} src={"./images/purchase.jpg"} />
+              <div className='hoverImg'>
+                <p>Geste</p>
+                <p>Gain carbone 9700/j</p>
+              </div>
+            </NavLink>
+            <NavLink to='/conseil' className='groopCard'>
+            <img className={"conseil"} src={"./images/purchase.jpg"} />
+            <div>
+              <div className='hoverImg'>
+                <p>Geste</p>
+                <p>Gain carbone 500/j</p>
+              </div>
+            </div>
+            </NavLink>
+            <NavLink to='/conseil' className='groopCard'>
+            <img className={"conseil"} src={"./images/purchase.jpg"} />
+            <div>
+              <div className='hoverImg'>
+                <p>Geste</p>
+                <p>Gain carbone 1500/j</p>
+              </div>
+            </div>
+            </NavLink>
+          </div>
         </div>
         <div className='valid'>
           <NavLink to="/conseil">
